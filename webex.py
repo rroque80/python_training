1b20dee8adf011a3945e223a1e269d0c1155958b
Reggie <reroque@cisco.com>
2016-02-04 16:03:23 -0800

    Added if statement tests

--- webex.py
+++ webex.py
@@ -1,2 +1,9 @@
 #!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 print ("hello world")
+
+if (1 == 1):
+	print ("hello world")
+if (1 == 2):
+	print ("I shouldn't print")
+else:
+	print ("It didn't print")
